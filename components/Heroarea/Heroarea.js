09e4eab58f2199003da47c932e9540553661f1d7
nasir <Nasir Uddin>
2022-08-31 09:41:13 +0600

    hero area fixed

--- components/Heroarea/Heroarea.js
+++ components/Heroarea/Heroarea.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const Heroarea = () => {
   return (
-    <div>
-     <section className="relative bg-[url('/pictwo.jpg')] bg-cover h-500 w-500">
+    <div className="bg-[url('/pictwo.jpg')] bg-cover">
+     <section className="relative  h-500 w-500 bg-indigo-900 opacity-95">
   {/* <img
     className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
     src="https://unsplash.com/photos/Hcfwew744z4"
@@ -15,23 +15,23 @@ const Heroarea = () => {
 
   <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
     <div className="max-w-xl text-center sm:text-left">
-      <h1 className="text-3xl font-extrabold sm:text-5xl">
+      <h1 className="text-3xl text-white font-extrabold sm:text-5xl">
         RESOURCES
-        <strong className="font-extrabold text-blue-900 mt-6 sm:block">
+        <strong className="font-extrabold text-white mt-6 sm:block">
           Forever Home.
         </strong>
       </h1>
 
-      <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!
+      <p className="max-w-lg mt-8 sm:leading-relaxed sm:text-xl text-white">
+      The book resource is essentially a series of web pages in a book-like format, with chapters, subchapters, and a table of contents.
       </p>
 
       <div className="flex flex-wrap gap-4 mt-8 text-center">
-        <a className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-blue-900 sm:w-auto active:bg-blue-900 hover:bg-blue-900 focus:outline-none focus:ring" href="/get-started">
+        <a className="block w-full px-12 py-3 text-sm font-medium text-indigo-900 rounded shadow bg-white sm:w-auto  hover:border-current focus:outline-none focus:ring" href="#">
           Get Started
         </a>
 
-        <a className="block w-full px-12 py-3 text-sm font-medium bg-white rounded shadow text-blue-900 sm:w-auto hover:text-blue-900 active:text-blue-900 focus:outline-none focus:ring" href="/about">
+        <a className="block w-full px-12 py-3 text-sm font-medium bg-white rounded shadow text-indigo-900 sm:w-auto hover:text-blue-900 active:text-blue-900 focus:outline-none focus:ring" href="#">
           Learn More
         </a>
       </div>
